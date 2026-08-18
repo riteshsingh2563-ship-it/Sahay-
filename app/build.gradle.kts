df62a13c0ec6c1f6f692ae1fa68d230302d08a6c
riteshsingh2563-ship-it <riteshsingh2563@gmail.com>
2026-08-18 18:48:01 +0530

    Update2 build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.google.devtools.ksp)
-    alias(libs.plugins.rorazzi)
     alias(libs.plugins.secrets)
     alias(libs.plugins.google.services)
 }
